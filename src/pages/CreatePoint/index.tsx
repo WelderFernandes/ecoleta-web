e67f4167359d4a6443c15cccbf992c45e2bb2cff
Welder n. fernandes <welderx3@gmaiil.com>
2020-07-03 16:23:06 -0300

    Final version

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -9,6 +9,8 @@ import axios from 'axios';
 import {LeafletMouseEvent} from 'leaflet';
 import { ReactSVG } from 'react-svg'
 
+import DropZone from '../../components/dropzone'
+
 interface Item {
     id: number;
     name: string;
@@ -42,7 +44,9 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('o')
     const [selectedItem, setselectedItem] = useState<number[]>([])
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0])
-    
+
+    const [selectedFile, setselectedFile] = useState<File>()
+
     const history = useHistory();
 
     useEffect(() => {
@@ -67,6 +71,7 @@ const CreatePoint = () => {
             setUfs(ufInitials)
         })
     }, []);
+
     useEffect(() => {
         if (selectedUf === '0') {
             return
@@ -120,22 +125,29 @@ const CreatePoint = () => {
    async function hundleSubmit(event: FormEvent) {
         event.preventDefault();
         
+        console.log(selectedFile);
+
         const { name , email, whatsapp } = formData;
         const uf = selectedUf;
         const city = selectedCity;
         const [latitude, longitude] = selectedPosition;
         const items = selectedItem;
 
-        const data = 
-           { name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+        const data = new FormData();
+        data.append('name',name);
+        data.append('email',email);
+        data.append('whatsapp',whatsapp);
+        data.append('uf',uf);
+        data.append('city',city);
+        data.append('latitude',String(latitude));
+        data.append('longitude',String(longitude));
+        data.append('items',items.join(','));
+        
+        if(selectedFile){
+            data.append('image', selectedFile);
         }
+
+
         await api.post('points', data)
         
        alert('Ponto de coleta criado')
@@ -152,7 +164,9 @@ const CreatePoint = () => {
             </header>
             <form onSubmit={hundleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
-                
+
+                <DropZone onFileUpload={setselectedFile} />
+
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
